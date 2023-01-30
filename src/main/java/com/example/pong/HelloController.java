74aa318dab1f4d7cc99befd21421258a0afc70e4
Erikces <114468760+Erikces@users.noreply.github.com>
2023-01-30 10:29:00 +0100

    Pong Commit

---
+++ src/main/java/com/example/pong/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.pong;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    public void initialize() {
+        //ball.getScene().setOnKeyPressed(keyEvent -> {
+        //
+        // if(keyEvent.getCode == KeyCode.UP){
+        // Raqueta.setY(Raqueta.getY() - 10);
+        //}
+        // })
+    }
+}
